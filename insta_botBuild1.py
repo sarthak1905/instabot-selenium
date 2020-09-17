78f623e509006df04c6f1ab5ff119f2844b3ff1a
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-17 17:51:40 +0530

    Create insta_botBuild1.py

---
+++ insta_botBuild1.py
@@ -0,0 +1,43 @@
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from getpass import getpass
+import time
+
+class InstaBot:
+    def __init__(self):
+        self.PATH = "C:\Program Files (x86)\chromedriver.exe"
+        self.driver = webdriver.Chrome(self.PATH)
+    
+    def start(self):
+        self.driver.get('https://www.instagram.com/')
+        time.sleep(3)
+
+    def login(self,username,pw):
+        self.username = username
+        self.pw = pw
+        user_field = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input')
+        pw_field = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')
+        login_button = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button/div')
+        user_field.send_keys(self.username)
+        pw_field.send_keys(self.pw)
+        login_button.click()
+        time.sleep(3)
+        not_now1 = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
+        not_now1.click()
+        time.sleep(2)
+        not_now2 = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+        not_now2.click()
+        time.sleep(1000)
+
+def main():
+    bb8 = InstaBot()
+    bb8.start()
+    username = input('Enter your username:')
+    pw = getpass()
+    bb8.login(username,pw)
+
+if __name__ =='__main__':
+    main()
