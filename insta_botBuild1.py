a0655590e92427c147c202d9e902f4bb38aaea12
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-18 21:02:07 +0530

    Updated documentation

--- insta_botBuild1.py
+++ insta_botBuild1.py
@@ -2,19 +2,25 @@ from selenium import webdriver
 from getpass import getpass
 import time
 
+#Class for the bot
 class InstaBot:
+
     def __init__(self):
         self.PATH = "C:\Program Files (x86)\chromedriver.exe"
         self.driver = webdriver.Chrome(self.PATH)
     
+    #Starts Instagram
     def start(self):
         self.driver.get('https://www.instagram.com/')
         time.sleep(2)
         return
 
+    #Logs into your account, also closes various dialogue boxes that open on the way 
     def login(self,username,pw):
+
         self.username = username
         self.pw = pw
+
         user_field = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
         pw_field = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
         login_button = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
@@ -30,17 +36,20 @@ class InstaBot:
         time.sleep(1)
         return
 
+    #Opens your profile
     def open_profile(self):
         profile_link = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a')
         profile_link.click()
         time.sleep(2)
         return
 
+    #Opens the list of the people you follow
     def open_following(self):
         following_link = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[3]/a')
         following_link.click()
         return
 
+    #Scrolls the list of your following and retrieves their names
     def scroll_list_following(self):
         SCROLL_PAUSE_TIME = 1
 
@@ -62,8 +71,12 @@ class InstaBot:
         return
 
 def main():
+
+    #Inputs username and password of your account 
     username = input('Enter your username:')
     pw = getpass('Enter your password(will NOT appear as you type):')
+
+    #Actual bot working 
     bb8 = InstaBot()
     bb8.start()
     bb8.login(username,pw)
